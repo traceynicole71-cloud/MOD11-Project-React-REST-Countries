abe848c4bf13fcec070dfd6d8b3543f26473374c
Tracey Roberts <traceynicole71@yahoo.com>
2026-05-05 14:16:02 -0400

    BorderTag edit

--- src/components/country/BorderTag.tsx
+++ src/components/country/BorderTag.tsx
@@ -10,13 +10,22 @@ const BorderTag: React.FC<BorderTagProps> = ({ code }) => {
     const [fullName, setFullName] = useState<string>(code);
 //Limit fields to name to keep network requests light
     useEffect(() => {
-const fetchBorderName = async () => {
+const controller = new AbortController();
+
+        const fetchBorderName = async () => {
     try {
         const response = await fetch(
           `https://restcountries.com/v3.1/alpha/${code}?fields=name`  
         );
 
+<<<<<<< HEAD
         if (!response.ok) {
+=======
+        if (response.ok) {
+            const data = await response.json();
+            setFullName(data.name.common);
+        } else {
+>>>>>>> f01e01c (BorderTag edit)
             throw new Error(`Failed to fetch border name: ${response.status}`);
         }
 
@@ -32,6 +41,7 @@ const fetchBorderName = async () => {
 console.error(`Could not fetch name for border: ${code}`, error);
     }
 };
+
 fetchBorderName();
     }, [code]);
 
