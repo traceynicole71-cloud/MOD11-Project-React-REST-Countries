555253e0ac69d5ce6e91e1277622e29987564559
keokistevenson <keokistevenson@gmail.com>
2026-05-06 02:01:00 -0400

    Fix theme provider import paths

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
-import { ThemeProvider } from "./context/ThemeContext";
+import { ThemeProvider } from "./context/themeProvider";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
