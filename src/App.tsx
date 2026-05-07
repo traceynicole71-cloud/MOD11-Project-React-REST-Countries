1a2a20dfb1a991d27d4baa50804a394b5ab82f0f
Monica Davila <monica.davila510@gmail.com>
2026-05-06 23:11:53 -0500

    Fix countries provider and API fetch

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,29 @@
- // ROOT of the component tree
+// ROOT of the component tree
 
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import { ThemeProvider } from "./context/themeProvider";
-
+import { CountriesProvider } from "./hooks/useCountries";
 import Home from "./pages/Home";
 import Detail from "./pages/Detail";
 
 import LayoutWrapper from "./components/layout/LayoutWrapper";
 
 function App() {
-  return (
-    <BrowserRouter>
-      <ThemeProvider>
-        <Routes>
-          <Route path="/" element={<LayoutWrapper />}>
-            <Route index element={<Home />} />
-            <Route path="country/:countryCode" element={<Detail />} />
-          </Route>
-        </Routes>
-      </ThemeProvider>
-    </BrowserRouter>
-  );
+  return (
+    <BrowserRouter>
+      <ThemeProvider>
+        <CountriesProvider>
+          <Routes>
+            <Route path="/" element={<LayoutWrapper />}>
+              <Route index element={<Home />} />
+              <Route path="country/:countryCode" element={<Detail />} />
+            </Route>
+          </Routes>
+        </CountriesProvider>
+      </ThemeProvider>
+    </BrowserRouter>
+  );
 }
 
 export default App;
